e1e3ad2fe2a7309d517ceea14332c24ee3138e65
farhandzakyarvianto <farhandzaky25@gmail.com>
2020-08-01 14:23:32 +0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Banner from "../components/Banner";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <Hero>
+            <Banner
+                title="Luxurious rooms"
+                subtitle="deluxe rooms starting at $299"
+            >
+                <Link to="/rooms" className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
+    );
+};
+
+export default Home;
